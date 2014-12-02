a13b5c5e067a7fed081e07986575dc61298efc49
sofia ma <sofia.ma@gmail.com>
2014-12-01 22:53:48 -0500

    edited show page, added date and time info with venue and location

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,8 +11,8 @@ class Event < ActiveRecord::Base
 
   def date_normalizer
     month = self.date.strftime('%b').downcase
-    day = self.date.strftime('%d')
-    "#{month}+#{day}"
+    date = self.date.strftime('%d')
+    "#{month}+#{date}"
   end
 
   def get_seatgeek_info
@@ -27,10 +27,23 @@ class Event < ActiveRecord::Base
 
     @results["events"].each do |e|
       event = {}
+      
       event[:title] = e["title"]
+    
       event[:datetime_local] = e["datetime_local"]
+      # date in sun dec 7, 2014 format
+      date = Date.parse(e["datetime_local"])
+      event[:date] = date.strftime('%a') + ' ' + date.strftime('%b') + ' ' + date.strftime('%-d') + ', ' + date.strftime('%Y')
+
+      # in 12hr AM:PM format
+      event[:local_time] = Time.parse(e["datetime_local"]).strftime('%l:%M %p')
+
       event[:lat] = e["venue"]["location"]["lat"]
       event[:long] = e["venue"]["location"]["lon"]
+      
+      event[:venue_name] = e["venue"]["name"]
+      event[:venue_loc] = e["venue"]["display_location"]
+      
       all_events << event
     end
 
