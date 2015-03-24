01fc7809e0d5b6621a52229a0943c2e5e6f76407
sofia ma <sofia.ma@gmail.com>
2015-03-23 21:40:36 -0400

    added leaflet awesome markers and made changes to marker icons

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,31 +1,26 @@
 $(document).ready(function(){
 
-  var foodMarker = L.AwesomeMarkers.icon({
-    icon: 'coffee',
-    markerColor: 'red'
-  });
-
   ticketlat = $('.ticket-info').data('temp').lat 
   ticketlong = $('.ticket-info').data('temp').long;
 
   ticketName = $('.ticket-info').data('temp').title;
 
-  L.marker([ticketlat, ticketlong]).addTo(map)
+  L.marker([ticketlat, ticketlong], {icon: L.AwesomeMarkers.icon({icon: 'star', markerColor: 'orange', prefix: 'fa'})}).addTo(map)
       .bindPopup(ticketName).openPopup();
 
 
   ($('.food-info').data('temp')).forEach(function(food){
-    L.marker([food.lat,food.long]).addTo(map)
+    L.marker([food.lat,food.long], {icon: L.AwesomeMarkers.icon({icon: 'cutlery', markerColor: 'red', prefix: 'fa'})}).addTo(map)
       .bindPopup(food.name);
   });
 
   ($('.night-info').data('temp')).forEach(function(night){
-    L.marker([night.lat,night.long]).addTo(map)
+    L.marker([night.lat,night.long], {icon: L.AwesomeMarkers.icon({icon: 'glass', markerColor: 'purple', prefix: 'fa'})}).addTo(map)
       .bindPopup(night.name)
   });
 
   ($('.sight-info').data('temp')).forEach(function(sight){
-    L.marker([sight.lat,sight.long]).addTo(map)
+    L.marker([sight.lat,sight.long], {icon: L.AwesomeMarkers.icon({icon: 'building', markerColor: 'blue', prefix: 'fa'})}).addTo(map)
       .bindPopup(sight.name)
   });
 
